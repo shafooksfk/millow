025654250dba961a03b33e268e6ecc68788bba50
Shafook <56448678+shafooksfk@users.noreply.github.com.>
2024-01-28 09:16:49 +0530

    feat: added navbar and added wallet connection

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,40 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
-
-}
+  const connectAccount = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  };
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="">Buy</a>
+        </li>
+        <li>
+          <a href="">Rent</a>
+        </li>
+        <li>
+          <a href="">Sell</a>
+        </li>
+      </ul>
+      <div className="nav__brand">
+        <img src={logo} alt="logo" />
+        <h1>Millow</h1>
+      </div>
+      {!account ? (
+        <button className="nav__connect" type="button" onClick={connectAccount}>
+          Connect
+        </button>
+      ) : (
+        <button className="nav__connect" type="button">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
