d7120d7417dbd58254b2137aaea57af94558c9fa
shafook <Muhammed.keloth>
2024-01-30 16:13:52 +0530

    added listing property

--- src/App.js
+++ src/App.js
@@ -15,9 +15,46 @@ import config from "./config.json";
 
 function App() {
   const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [homes, setHomes] = useState(null);
+
   const loadBlockChainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
+    const network = await provider.getNetwork();
+    // console.log(
+    //   config[network.chainId].realEstate.address,
+    //   "reals esttate address");
+
+    // connect smart contract as javascript
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider
+    );
+    const totalSupply = await realEstate.totalSupply();
+    // console.log(totalSupply.toString());
+
+    // fetch and save to an array for listing
+    const homes = [];
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+
+    setHomes(homes);
+    console.log(homes);
+
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider
+    );
+    setEscrow(escrow);
     // on account changed
     window.ethereum.on("accountsChanged", async () => {
       const accounts = await window.ethereum.request({
@@ -30,13 +67,32 @@ function App() {
 
   useEffect(() => {
     loadBlockChainData();
-  }, [window.ethereum]);
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
-        <h3>Welcome to Millow</h3>
+        <h3>Homes for you</h3>
+        <hr />
+        <div className="cards">
+          {homes && homes.map((home, i) => (
+            <div className="card" key={i}>
+              <div className="card__image">
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong>bds |<strong>{home.attributes[3].value}</strong>ba |
+                  <strong>{home.attributes[4].value}</strong>sqft |
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
